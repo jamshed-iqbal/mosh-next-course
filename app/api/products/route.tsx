f5646faa591a2c37c52cd4f7b0965e1092cd485a
Jamshed Iqbal <jamshediqbal84@gmail.com>
2024-02-05 22:42:43 +0500

    product

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,8 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Milk", price: 2.5 },
-    { id: 2, name: "Bread", price: 3.5 },
-  ]);
+import schema from "@/app/api/products/schema";
+import prisma from "@/prisma/client";
+
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return NextResponse.json(products);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const validation = schema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(newProduct, { status: 201 });
 }
