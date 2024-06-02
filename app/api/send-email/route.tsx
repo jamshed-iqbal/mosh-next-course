9ca5737bd16cebbc06e39e3a43fc14369d8a017a
Jamshed Iqbal <jamshediqbal84@gmail.com>
2024-06-03 02:33:52 +0500

    send email commented out code

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -3,18 +3,18 @@
 "use client";
 
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
-import WelcomeTemplate from "@/emails/WelcomeTemplate";
+// import { Resend } from "resend";
+// import WelcomeTemplate from "@/emails/WelcomeTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "jamshediqbal84@gmail.com",
-    subject: "Hello World",
-    react: <WelcomeTemplate name="Jamshed Iqbal" />,
-  });
+  // await resend.emails.send({
+  //   from: "onboarding@resend.dev",
+  //   to: "jamshediqbal84@gmail.com",
+  //   subject: "Hello World",
+  //   react: <WelcomeTemplate name="Jamshed Iqbal" />,
+  // });
 
   return NextResponse.json({});
 }
